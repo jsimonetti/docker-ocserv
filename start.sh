d9acaaddc503716b7df83eb0b09eae5529562f51
Jeroen Simonetti <jeroen@simonetti.nl>
2019-05-17 15:21:12 +0200

    Fix start script
    Signed-off-by: Jeroen Simonetti <jeroen@simonetti.nl>

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sysctl -w net.ipv4.conf.all.proxy_arp = 1
-exec /ocserv -c /etc/ocserv/ocserv.conf -f
+sysctl -w net.ipv4.conf.all.proxy_arp=1
+exec ocserv -c /etc/ocserv/ocserv.conf -f
 
